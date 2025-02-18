f484221c13aaa85b1e9b00e1edf38fe852abf0be
mohit-nagaraj <mohitnagaraj20@gmail.com>
2025-02-18 12:47:38 +0530

    basic logger

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import expressWinston from "express-winston";
+import logger from "./utils/logger.js";
 
 dotenv.config();
 
@@ -9,6 +11,17 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(expressWinston.logger({
+    winstonInstance: logger,
+    meta: true, 
+    msg: 'HTTP {{req.method}} {{req.url}}',
+    colorize: true, 
+}));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
